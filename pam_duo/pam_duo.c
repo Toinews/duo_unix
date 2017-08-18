c748407d88eb51fb8fa1961237f00bfc71a95966
Antoine Cardon <antoine.cardon@algolia.com>
2017-08-18 10:16:53 +0200

    Port pam_duo to libduo compatible with v2 API. Add different PAM_* error code regarding context.

--- pam_duo/pam_duo.c
+++ pam_duo/pam_duo.c
@@ -45,7 +45,7 @@
 #include <security/pam_modules.h>
 #endif
 #ifdef HAVE_SECURITY_PAM_EXT_H
-#include <security/pam_ext.h>	/* Linux-PAM */
+#include <security/pam_ext.h>   /* Linux-PAM */
 #endif
 
 /* OpenGroup RFC86.0 and XSSO specify no "const" on arguments */
@@ -65,9 +65,9 @@
 #endif
 
 #ifndef DUO_PRIVSEP_USER
-# define DUO_PRIVSEP_USER	"duo"
+# define DUO_PRIVSEP_USER   "duo"
 #endif
-#define DUO_CONF		DUO_CONF_DIR "/pam_duo.conf"
+#define DUO_CONF        DUO_CONF_DIR "/pam_duo.conf"
 
 static int
 __ini_handler(void *u, const char *section, const char *name, const char *val)
@@ -81,104 +81,84 @@ __ini_handler(void *u, const char *section, const char *name, const char *val)
     return (1);
 }
 
-static void
-__duo_status(void *arg, const char *msg)
-{
-    pam_info((pam_handle_t *)arg, "%s", msg);
-}
-
-static char *
-__duo_prompt(void *arg, const char *prompt, char *buf, size_t bufsz)
-{
-    char *p = NULL;
-
-    if (pam_prompt((pam_handle_t *)arg, PAM_PROMPT_ECHO_ON, &p,
-        "%s", prompt) != PAM_SUCCESS || p == NULL) {
-        return (NULL);
-    }
-    strlcpy(buf, p, bufsz);
-    free(p);
-    return (buf);
-}
-
 PAM_EXTERN int
 pam_sm_authenticate(pam_handle_t *pamh, int pam_flags,
     int argc, const char *argv[])
 {
-	struct duo_config cfg;
-	struct passwd *pw;
-	struct in_addr addr;
-	duo_t *duo;
-	duo_code_t code;
+    struct duo_config cfg;
+    struct passwd *pw = NULL;
+    struct in_addr addr;
+    duo_t *duo = NULL;
+    duo_auth_t auth = NULL;
 
-	/*
-	 * Only variables that will be passed to a pam_* function
-	 * need to be marked as 'duopam_const char *', anything else
-	 * should be 'const char *'. This is because there are different
-	 * PAM implementations, some with the const qualifier, and some
-	 * without.
-	 */
-	duopam_const char *ip, *service, *user;
-	const char *cmd, *p, *config, *host;
+    /*
+     * Only variables that will be passed to a pam_* function
+     * need to be marked as 'duopam_const char *', anything else
+     * should be 'const char *'. This is because there are different
+     * PAM implementations, some with the const qualifier, and some
+     * without.
+     */
+    duopam_const char *ip = NULL, *service = NULL, *user = NULL;
+    const char *config = NULL, *host = NULL;
 
-	int i, flags, pam_err, matched;
+    int i, pam_err, matched;
 
-	/*
-	 * Handle a delimited GECOS field. E.g.
-	 *
-	 *     username:x:0:0:code1/code2/code3//textField/usergecosparsed:/username:/bin/bash
-	 *
-	 * Parse the username from the appropriate position in the GECOS field.
-	 */
-	const char delimiter = '/';
-	const unsigned int delimited_position = 5;
+    /*
+     * Handle a delimited GECOS field. E.g.
+     *
+     *     username:x:0:0:code1/code2/code3//textField/usergecosparsed:/username:/bin/bash
+     *
+     * Parse the username from the appropriate position in the GECOS field.
+     */
+    const char delimiter = '/';
+    const unsigned int delimited_position = 5;
 
-	duo_config_default(&cfg);
+    duo_config_default(&cfg);
 
-	/* Parse configuration */
-	config = DUO_CONF;
-	for (i = 0; i < argc; i++) {
-		if (strncmp("conf=", argv[i], 5) == 0) {
-			config = argv[i] + 5;
-		} else if (strcmp("debug", argv[i]) == 0) {
-			duo_debug = 1;
-		} else {
-			duo_syslog(LOG_ERR, "Invalid pam_duo option: '%s'",
-			    argv[i]);
-			return (PAM_SERVICE_ERR);
-		}
-	}
-	i = duo_parse_config(config, __ini_handler, &cfg);
-	if (i == -2) {
-		duo_syslog(LOG_ERR, "%s must be readable only by user 'root'",
-		    config);
-		return (cfg.failmode == DUO_FAIL_SAFE ? PAM_SUCCESS : PAM_SERVICE_ERR);
-	} else if (i == -1) {
-		duo_syslog(LOG_ERR, "Couldn't open %s: %s",
-		    config, strerror(errno));
-		return (cfg.failmode == DUO_FAIL_SAFE ? PAM_SUCCESS : PAM_SERVICE_ERR);
-	} else if (i > 0) {
-		duo_syslog(LOG_ERR, "Parse error in %s, line %d", config, i);
-		return (cfg.failmode == DUO_FAIL_SAFE ? PAM_SUCCESS : PAM_SERVICE_ERR);
-	} else if (!cfg.apihost || !cfg.apihost[0] ||
+    /* Parse configuration */
+    config = DUO_CONF;
+    for (i = 0; i < argc; i++) {
+        if (strncmp("conf=", argv[i], 5) == 0) {
+            config = argv[i] + 5;
+        } else if (strcmp("debug", argv[i]) == 0) {
+            duo_debug = 1;
+        } else {
+            duo_syslog(LOG_ERR, "Invalid pam_duo option: '%s'",
+                argv[i]);
+            return (PAM_SERVICE_ERR);
+        }
+    }
+    i = duo_parse_config(config, __ini_handler, &cfg);
+    if (i == -2) {
+        duo_syslog(LOG_ERR, "%s must be readable only by user 'root'",
+            config);
+        return (cfg.failmode == DUO_FAIL_SAFE ? PAM_SUCCESS : PAM_NO_MODULE_DATA);
+    } else if (i == -1) {
+        duo_syslog(LOG_ERR, "Couldn't open %s: %s",
+            config, strerror(errno));
+        return (cfg.failmode == DUO_FAIL_SAFE ? PAM_SUCCESS : PAM_NO_MODULE_DATA);
+    } else if (i > 0) {
+        duo_syslog(LOG_ERR, "Parse error in %s, line %d", config, i);
+        return (cfg.failmode == DUO_FAIL_SAFE ? PAM_SUCCESS : PAM_NO_MODULE_DATA);
+    } else if (!cfg.apihost || !cfg.apihost[0] ||
             !cfg.skey || !cfg.skey[0] || !cfg.ikey || !cfg.ikey[0]) {
         duo_syslog(LOG_ERR, "Missing host, ikey, or skey in %s", config);
-        return (cfg.failmode == DUO_FAIL_SAFE ? PAM_SUCCESS : PAM_SERVICE_ERR);
+        return (cfg.failmode == DUO_FAIL_SAFE ? PAM_SUCCESS : PAM_NO_MODULE_DATA);
     }
 
     /* Check user */
     if (pam_get_user(pamh, &user, NULL) != PAM_SUCCESS ||
         (pw = getpwnam(user)) == NULL) {
-        close_config(&cfg);
-        return (PAM_USER_UNKNOWN);
+        duo_log(LOG_WARNING, "Unknown user.", user, "", "");
+        pam_err = PAM_USER_UNKNOWN;
+        goto cleanup;
     }
     /* XXX - Service-specific behavior */
-    flags = 0;
-    cmd = NULL;
     if (pam_get_item(pamh, PAM_SERVICE, (duopam_const void **)
         (duopam_const void *)&service) != PAM_SUCCESS) {
-        close_config(&cfg);
-        return (PAM_SERVICE_ERR);
+        duo_syslog(LOG_ERR, "Unknown error");
+        pam_err = PAM_SERVICE_ERR;
+        goto cleanup;
     }
     if (strcmp(service, "sshd") == 0) {
         /*
@@ -186,25 +166,25 @@ pam_sm_authenticate(pam_handle_t *pamh, int pam_flags,
          * OpenSSH accumulates PAM_TEXT_INFO from modules to send in
          * an SSH_MSG_USERAUTH_BANNER post-auth, not real-time!
          */
-        flags |= DUO_FLAG_SYNC;
-    } else if (strcmp(service, "sudo") == 0) {
-        cmd = getenv("SUDO_COMMAND");
+        // flags |= DUO_FLAG_SYNC;
     } else if (strcmp(service, "su") == 0 || strcmp(service, "su-l") == 0) {
         /* Check calling user for Duo auth, just like sudo */
         if ((pw = getpwuid(getuid())) == NULL) {
-            close_config(&cfg);
-            return (PAM_USER_UNKNOWN);
+            duo_log(LOG_WARNING, "Unknown user.", user, "", "");
+            pam_err = PAM_USER_UNKNOWN;
+            goto cleanup;
         }
         user = pw->pw_name;
     }
+
     /* Check group membership */
     matched = duo_check_groups(pw, cfg.groups, cfg.groups_cnt);
     if (matched == -1) {
-        close_config(&cfg);
-        return (PAM_SERVICE_ERR);
+        pam_err = PAM_SERVICE_ERR;
+        goto cleanup;
     } else if (matched == 0) {
-        close_config(&cfg);
-        return (PAM_SUCCESS);
+        pam_err = PAM_SUCCESS;
+        goto cleanup;        
     }
 
     /* Use GECOS field if called for */
@@ -225,7 +205,6 @@ pam_sm_authenticate(pam_handle_t *pamh, int pam_flags,
     }
 
     /* Grab the remote host */
-    ip = NULL;
     pam_get_item(pamh, PAM_RHOST,
         (duopam_const void **)(duopam_const void *)&ip);
     host = ip;
@@ -240,68 +219,89 @@ pam_sm_authenticate(pam_handle_t *pamh, int pam_flags,
         }
     }
 
-    /* Try Duo auth */
-    if ((duo = duo_open(cfg.apihost, cfg.ikey, cfg.skey,
-                    "pam_duo/" PACKAGE_VERSION,
-                    cfg.noverify ? "" : cfg.cafile, cfg.https_timeout, cfg.http_proxy)) == NULL) {
-        duo_log(LOG_ERR, "Couldn't open Duo API handle", pw->pw_name, host, NULL);
-        close_config(&cfg);
-        return (PAM_SERVICE_ERR);
-    }
-    duo_set_conv_funcs(duo, __duo_prompt, __duo_status, pamh);
-
-    if (cfg.autopush) {
-        flags |= DUO_FLAG_AUTO;
+    /* Try Duo auth. */
+    if ((duo = duo_init(cfg.apihost, cfg.ikey, cfg.skey,
+                    "login_duo/" PACKAGE_VERSION,
+                    cfg.noverify ? "" : cfg.cafile,
+                    cfg.http_proxy)) == NULL) {
+        duo_log(LOG_ERR, "Couldn't open Duo API handle",
+            pw->pw_name, host, NULL);
+        pam_err = PAM_SERVICE_ERR;
+        goto cleanup;
     }
 
-    pam_err = PAM_SERVICE_ERR;
+    auth = duo_auth_check(duo);
+    if (auth == NULL || auth->stat != DUO_OK) {
+        /* Duo endpoint not available. */
+        pam_err = cfg.failmode == DUO_FAIL_SAFE ? PAM_SUCCESS : PAM_SERVICE_ERR;
+        goto cleanup;
+    }
+
+    auth = duo_auth_free(auth);
+    /* Perform preauth check */
+    auth = duo_auth_preauth(duo, user);
+    if (auth == NULL || auth->stat != DUO_OK) {
+        /* Duo endpoint not available. */
+        pam_err = cfg.failmode == DUO_FAIL_SAFE ? PAM_SUCCESS : PAM_SERVICE_ERR;
+        goto cleanup;
+    }
+
+    if (strcmp(auth->ok.preauth.result, "allow") == 0) {
+        /* No need to process to auth. User allowed to bypass */
+        duo_log(LOG_WARNING, "Skipped Duo login", user, host, auth->ok.preauth.status_msg);
+        pam_err = PAM_SUCCESS;
+        goto cleanup;
+    } else if (strcmp(auth->ok.preauth.result, "auth") == 0) {
+        /* Handle auth device and factor here ?*/
+        ;
+    } else {
+        /* enroll or deny, deny access */
+        duo_log(LOG_WARNING, "User not allowed to login.", user, host, auth->ok.preauth.status_msg);
+        pam_err = PAM_PERM_DENIED;
+        goto cleanup;
+    }
+
+    pam_err = PAM_PERM_DENIED;
 
     for (i = 0; i < cfg.prompts; i++) {
-        code = duo_login(duo, user, host, flags,
-                    cfg.pushinfo ? cmd : NULL);
-        if (code == DUO_FAIL) {
-            duo_log(LOG_WARNING, "Failed Duo login",
-                pw->pw_name, host, duo_geterr(duo));
-            if ((flags & DUO_FLAG_SYNC) == 0) {
-                pam_info(pamh, "%s", "");
-            }
-            /* Keep going */
-            continue;
+        if (auth) {
+            auth = duo_auth_free(auth);
         }
-        /* Terminal conditions */
-        if (code == DUO_OK) {
-            if ((p = duo_geterr(duo)) != NULL) {
-                duo_log(LOG_WARNING, "Skipped Duo login",
-                    pw->pw_name, host, p);
-            } else {
-                duo_log(LOG_INFO, "Successful Duo login",
-                    pw->pw_name, host, NULL);
-            }
-            pam_err = PAM_SUCCESS;
-        } else if (code == DUO_ABORT) {
-            duo_log(LOG_WARNING, "Aborted Duo login",
-                pw->pw_name, host, duo_geterr(duo));
-            pam_err = PAM_ABORT;
-        } else if (cfg.failmode == DUO_FAIL_SAFE &&
-                    (code == DUO_CONN_ERROR ||
-                     code == DUO_CLIENT_ERROR || code == DUO_SERVER_ERROR)) {
-            duo_log(LOG_WARNING, "Failsafe Duo login",
-                pw->pw_name, host, duo_geterr(duo));
-            pam_err = PAM_SUCCESS;
-        } else {
-            duo_log(LOG_ERR, "Error in Duo login",
-                pw->pw_name, host, duo_geterr(duo));
+
+        auth = duo_auth_auth(duo, user, "push", NULL, NULL);
+        if (auth == NULL || auth->stat != DUO_OK) {
+            /* Something went wrong with server and/or request */
             pam_err = PAM_SERVICE_ERR;
+            break;
         }
-        break;
+        if (strcmp(auth->ok.auth.result, "allow") == 0) {
+            duo_log(LOG_INFO, "Successful Duo login.", user, host, auth->ok.auth.status_msg);
+            pam_err = PAM_SUCCESS;
+            break;
+        }
+        else if (strcmp(auth->ok.auth.status, "fraud") == 0) {
+                /* Fraud detected. Report and do not continue with login */
+                duo_log(LOG_WARNING, "Aborted fraudulent Duo login. Incident reported.",
+                    user, host, auth->ok.preauth.status_msg);
+                pam_err = PAM_ABORT;
+                break;
+        } else {
+            /* Try again */
+            duo_log(LOG_WARNING, "Denied Duo login.", user, host, auth->ok.auth.status_msg);
+        }
     }
     if (i == MAX_PROMPTS) {
         pam_err = PAM_MAXTRIES;
     }
-    duo_close(duo);
-    close_config(&cfg);
 
-    return (pam_err);
+cleanup:
+    if (auth)
+        auth = duo_auth_free(auth);
+    if (duo)
+        duo = duo_close(duo);
+    close_config(&cfg);
+    
+    return pam_err;
 }
 
 PAM_EXTERN int
